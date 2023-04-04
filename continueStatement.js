19ae9ec85486ea63c09a2ef8474c393786a1bd9b
Mamunarrashid39 <mamunarrashid.cse@gmail.com>
2023-04-04 22:58:44 +0600

    j

--- continueStatement.js
+++ continueStatement.js
@@ -1,9 +1,20 @@
-// program to print the value of i
-for (let i = 1; i <= 6; i++) {
-  // condition to continue
-  if (i == 3) {
+let sum = 0;
+let number = 0;
+
+while (number >= 0) {
+  // add all positive numbers
+  sum += number;
+
+  // take input from the user
+  number = parseInt(prompt("Enter a number: "));
+
+  // continue condition
+  if (isNaN(number)) {
+    console.log("You entered a string.");
+    number = 0; // the value of number is made 0 again
     continue;
   }
-
-  console.log(i);
 }
+
+// display the sum
+console.log(`The sum is ${sum}.`);
